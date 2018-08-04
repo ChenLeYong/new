38ebe6ec4679812ee0bb8f9899d155fcd8609dd0
Chen Le <chenleyong97@Chens-MacBook-Pro-2.local>
2018-08-04 21:33:54 +0800

    new programme

--- HAHAHA.java
+++ HAHAHA.java
@@ -1,7 +1,7 @@
-public class HelloWorld{
+public class HAHAHA{
     public static void main(String[]args){
         System.out.println("Hello Chen Le");
         System.out.println("Chen Ming is here");
-        System.out.println("HAHAHA");
+        System.out.println("HAHAHA!");
     }
 }
